f0524c717ffdcd2736eb0f13b0c6b6699bd12e4b
Daniel Tham <dantham14@gmail.com>
2024-04-11 22:24:49 -0700

    more outputs

--- bot.py
+++ bot.py
@@ -3,29 +3,35 @@ import time
 from chatterbot.trainers import ListTrainer
 
 from chatterbot.trainers import ChatterBotCorpusTrainer
-import re
+import random
 
 time.clock = time.time
 
 # CORPUS_FILE = "./corpus.txt"
 
 train_data = []
+
+question_words = ['Hmm', 'I\'m thinking', 'I wondering...maybe']
+exclaimations = ['Why are you mad?', 'Relax a little bit...']
+
 chatbot = ChatBot(
     "LING 450 ChatBot",
     storage_adapter='chatterbot.storage.SQLStorageAdapter',
     logic_adapters=[
         {
             'import_path': 'chatterbot.logic.BestMatch',
-            'maximum_similarity_threshold': 0.60
+            'maximum_similarity_threshold': 0.70
         }
     ]
 )
 
+# loads clean corpus into array
 with open('./cleaned_corpus3.csv','r', encoding='utf-8') as f_in:
 
     for line in f_in: 
         train_data.append(line)
 
+# creates bot instance
 trainer = ListTrainer(chatbot)
 
 # run this to retrain
@@ -37,10 +43,17 @@ while True:
     query = input("> ")
     if query in exit_conditions:
         break
+    
     # addresses the one letter response from learning
     elif len(str(chatbot.get_response(query))) == 1:
         print(f" Oops I'm still learning!")
+        
+    # if a statement ends in !
+    elif query.endswith("!"):
+        print(f" {random.choice(exclaimations)}...but... {chatbot.get_response(query)}")
+    
+    # if a statement ends in ?
     elif query.endswith("?"):
-        print(f" Hmm... {chatbot.get_response(query)}")
+        print(f" {random.choice(question_words)}... {chatbot.get_response(query)}")
     else:
         print(f" {chatbot.get_response(query)}")
